fb3d8b2fee3ce55c4b515b50ede55a74ccf75d5c
Rebecca208 <71957619@itiscastelli.onmicrosoft.com>
2025-11-25 11:47:59 +0100

    "Struttura piano"

--- src/it/game/pianoforte/Negro.java
+++ src/it/game/pianoforte/Negro.java
@@ -1,9 +1,8 @@
 package it.game.pianoforte;
 import com.raylib.Colors;
-import com.raylib.Raylib.Color;
 
 public class Negro extends Tasto{
-    public Negro(String nota, boolean premuto, Color colorePremuto, Color coloreNormale, int WIDTH, int HEIGHT, Color colorePremuto1, Color coloreNormale1, int WIDTH1, int HEIGHT1) {
+    public Negro(String nota, boolean premuto, Colors colorePremuto, Colors coloreNormale, int WIDTH, int HEIGHT, Colors colorePremuto1, Colors coloreNormale1, int WIDTH1, int HEIGHT1) {
         super(nota, premuto, colorePremuto, coloreNormale, WIDTH, HEIGHT);
         setColorePremuto(Colors.GRAY);
         setColoreNormale(Colors.BLACK);
